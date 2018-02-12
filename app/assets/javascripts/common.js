f442393bfa01dde7f5f301ba9410998d70ed7e86
HugoMitsumori <hugo.mitsumori@hotmail.com>
2018-02-11 22:34:03 -0200

    fix javascript loading

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).on('turbolinks:load', function() {
   $('#login-form').on('submit', function() {
     $('#session-message').append('Entrando. Aguarde...');
   });
