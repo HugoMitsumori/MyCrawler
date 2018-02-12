4d69812a5dcdfc22b9e5cc5b6d8e08d253f4377f
HugoMitsumori <hugo.mitsumori@hotmail.com>
2018-02-11 22:42:47 -0200

    comment unused files for heroku deploy

--- app/helpers/bs.rb
+++ app/helpers/bs.rb
@@ -1,25 +1,25 @@
-load 'crawler.rb'
+# load 'crawler.rb'
 
-URL_BS = 'https://extra2.bsgi.org.br/impressos/online/edicao/bs/'.freeze
+# URL_BS = 'https://extra2.bsgi.org.br/impressos/online/edicao/bs/'.freeze
 
-crawler = Crawler.new
+# crawler = Crawler.new
 
-crawler.login ARGV[0], ARGV[1]
+# crawler.login ARGV[0], ARGV[1]
 
-page = crawler.agent.get URL_BS
-links = []
-content = page.search("//div[@class='ui big list']")
-content.search('//a[@class="item color-grey"]').map { |link| links << link['href'] }
+# page = crawler.agent.get URL_BS
+# links = []
+# content = page.search("//div[@class='ui big list']")
+# content.search('//a[@class="item color-grey"]').map { |link| links << link['href'] }
 
-output = File.new 'saida.html', 'w+'
-output.puts "<!DOCTYPE html>\n<html>\n<body>\n<meta charset=\"utf-8\" />"
+# output = File.new 'saida.html', 'w+'
+# output.puts "<!DOCTYPE html>\n<html>\n<body>\n<meta charset=\"utf-8\" />"
 
-content = page.search('//h2')
-output.puts content
+# content = page.search('//h2')
+# output.puts content
 
-links.each do |link|
-  content = crawler.extract_article URL + link
-  output.puts content
-end
-output.puts "</body>\n</html>"
-output.close
+# links.each do |link|
+#   content = crawler.extract_article URL + link
+#   output.puts content
+# end
+# output.puts "</body>\n</html>"
+# output.close
