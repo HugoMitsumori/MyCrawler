dfb5f23980a28049855b870dfcd6520de4242530
Hugo Mitsumori <hugo@rdscodes.com.br>
2019-06-02 01:13:32 -0300

    add support for different part names

--- app/lib/serie_extractor.rb
+++ app/lib/serie_extractor.rb
@@ -31,6 +31,7 @@ class SerieExtractor
     @final_edition
     @current_part = 0
     @chapters = []
+    @chapter_first_editions = []
     @current_chapter = ''
     @file = File.new(file_name + '.html', 'w+')
     @content = {}
@@ -42,7 +43,7 @@ class SerieExtractor
     @file.puts "<i>Brasil Seikyo, Edições #{@initial_edition} a #{@final_edition}</i>\n"
 
     for i in @final_page.downto @initial_page
-      puts "= Processando #{URL_NRH + i.to_s}"
+      puts "= Acessando página #{URL_NRH + i.to_s}"
       @crawler.agent.visit(URL_NRH + i.to_s)
       page = @crawler.agent
       page = Nokogiri::HTML(page.html).search("//div[@class='content']").remove
@@ -74,46 +75,61 @@ class SerieExtractor
     @initial_edition, @final_edition = file.gets.split.map { |x| Integer(x) }
     while (chapter = file.gets)
       chapter = chapter.delete("\n").upcase
-      @chapters << chapter
-      @content[chapter] = []
+      chapter_name = chapter.split(' ', 2).last
+      @chapters << chapter_name
+      @chapter_first_editions << chapter.split.first.to_i
+      @content[chapter_name] = []
     end
   end
 
   def process_edition (edition)
     edition_title = edition.content.gsub('  ', '').gsub(/[\n]+/, "\n").lines[1]
-    edition_number = Integer(edition_title.split[1])
-    if edition_number.between?(@initial_edition, @final_edition)
-      edition.children[3].children.each do |part|
-        if part.class == Nokogiri::XML::Text
-          # eliminates empty nodes
-          next
-        end
+    edition_number = edition_title.split[1].to_i
+    return unless edition_number.between?(@initial_edition, @final_edition)
 
-        link = part.attribute_nodes[1].value
-        part_name = part.attribute_nodes[3].value.upcase
-        temp_part_number = part_name.scan(/[\(][0-9]+[\)]/)[0]&.delete('()')
-        if temp_part_number
-          part_number = Integer(temp_part_number)
-        else
-          part_number = Integer(edition.search('.header.blue')[0]&.text.strip.split[1])
-        end
+    edition.children[3].children.each do |part|
+      next if part.class == Nokogiri::XML::Text || part.text.strip.upcase == 'CAPA'
+      # eliminates empty nodes
 
-        @chapters.each do |chapter|
-          unless part_name.include?(chapter)
-            next
-          end
-          if @current_chapter != chapter
-            @current_chapter = chapter
-            puts "=====Novo capítulo: #{@current_chapter}====="
-            @current_part = 1
-          end
-          part_url = Crawler::BASE_URL + link
-          puts "processando #{part_name} de #{part_url}"
-          @crawler.agent.visit part_url
-          content = substitute_words(ArticlesExtractor.extract_text(@crawler.agent).to_s)
-          @content[chapter][part_number] = content
-        end
+      link = part[:href]
+      part_name = part.attribute_nodes[3].value.upcase
+      temp_part_number = part_name.scan(/[\(][0-9]+[\)]/)[0]&.delete('()')
+      if temp_part_number
+        part_number = temp_part_number.to_i
+      else
+        part_number = edition.search('.header.blue')[0]&.text.strip.split[1].to_i
       end
+
+      # when more than one part page per edition
+      # @chapters.each do |chapter|
+      #   unless part_name.include?(chapter)
+      #     next
+      #   end
+      #   if @current_chapter != chapter
+      #     @current_chapter = chapter
+      #     puts "=====Novo capítulo: #{@current_chapter}====="
+      #     @current_part = 1
+      #   end
+      #   part_url = Crawler::BASE_URL + link
+      #   puts "processando #{part_name} de #{part_url}"
+      #   @crawler.agent.visit part_url
+      #   content = substitute_words(ArticlesExtractor.extract_text(@crawler.agent).to_s)
+      #   @content[chapter][part_number] = content
+      # end
+
+
+      # check if new chapter
+      if @chapter_first_editions.include? edition_number
+        @current_chapter = @chapters[@chapter_first_editions.index(edition_number)]
+        puts "=====Novo capítulo: #{@current_chapter}====="
+        @current_part = 1
+      end
+
+      part_url = Crawler::BASE_URL + link
+      puts "processando #{part_name} de #{part_url}"
+      @crawler.agent.visit part_url
+      content = substitute_words(ArticlesExtractor.extract_text(@crawler.agent).to_s)
+      @content[@current_chapter][part_number] = content
     end
   end
 
